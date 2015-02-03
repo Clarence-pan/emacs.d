ff59f8629b4c994ba8ad2d0dcec21c713fefd191
panchangyun <panchangyun@tuniu.com>
2015-02-03 10:49:18 +0800

    My emacs config

---
+++ init.el
@@ -0,0 +1,27 @@
+;; -*- coding: utf-8 -*-
+;; M-x eval-buffer to set this configuration valid.
+
+;; record start time for analyze performance
+(setq emacs-load-start-time (current-time))
+
+;; color and background
+(set-background-color "white")
+(set-foreground-color "black")
+(set-face-foreground 'region "green")
+(set-face-background 'region "blue")
+
+;; start server mode
+(server-start)
+
+;; set plugin path
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
+
+;; Which functionality to enable (use t or nil for true and false)
+(setq *win32* (eq system-type 'windows-nt) )
+
+
+;; remove the start page
+(kill-buffer "*GNU emacs*")
+
+
+
